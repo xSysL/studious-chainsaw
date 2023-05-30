b6a9533531130da5b6aea0ff581fdbfd527451ae
xSysL <subhankarlayek5@gmail.com>
2023-05-31 01:40:35 +0530

    commit

--- testcode3.py
+++
@@ -1,9 +0,0 @@
-import random
-from turtle import Turtle, Screen
-tim = Turtle()
-tim.shape("classic")
-tim.color("red")
-tim.pensize(10)
-walks = [tim.lt(90), tim.rt(90), tim.back(10)]
-
-print(random.choice(walks))
