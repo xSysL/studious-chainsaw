7c04519dee4290a93cc6797270cf4ffd66d84c79
xSysL <subhankarlayek5@gmail.com>
2023-06-01 19:55:15 +0530

    commit

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -2,7 +2,7 @@ import pandas
 
 data = pandas.read_csv("NATO Alphabet\\nato_phonetic_alphabet.csv")
 nato = {row.letter: row.code for (index, row) in data.iterrows()}
-print(nato)
+# print(nato)
 
 word = input("Enter a word: ").upper()
 output_list = [nato[letter] for letter in word]
